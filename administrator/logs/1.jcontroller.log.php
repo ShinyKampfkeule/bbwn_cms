6751ab3c8ce6ce08d2f5e3b2d138a1cc9451a9f1
ShinyKampfkeule <Kevin.Lehmann@capitalbay.de>
2022-07-04 19:41:44 +0200

    Readd all git-ignore things

---
+++ administrator/logs/1.jcontroller.log.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2022-06-07 08:22:54 UTC
+#Software: Joomla! 4.1.3 Stable [ Kuamini ] 10-May-2022 16:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2022-06-07T08:22:54+00:00	INFO ::1	controller	Holding edit ID com_categories.edit.category.8 Array (     [0] => 8 ) 
+2022-06-07T08:22:56+00:00	INFO ::1	controller	Checking edit ID com_categories.edit.category.8: 1 Array (     [0] => 8 ) 
