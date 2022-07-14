db08e4c75ff105bfb794e84e83e0f71b831f607d
ShinyKampfkeule <Kevin.Lehmann@capitalbay.de>
2022-07-14 16:19:09 +0200

    Update bigscreen_layout-with-person.php

--- templates/j4starter/html/com_content/category/bigscreen_layout-with-person.php
+++ templates/j4starter/html/com_content/category/bigscreen_layout-with-person.php
@@ -2,17 +2,15 @@
   $url_name = $_SERVER['SERVER_NAME']; 
 
   if ( $url_name !== "localhost" ) {
-    $link_name = "/kevin";
-  } else {
-    $link_name = "";
+    $url_name = "{$url_name}/kevin";
   }
   $bgimg_id = json_decode($article->jcfields[15]->rawvalue)->itemId;
   $bgimg_name = json_decode($article->jcfields[15]->rawvalue)->filename;
-  $bgimg = "{$link_name}/images/econa/fields/15/com_content_article/{$bgimg_id}/{$bgimg_name}_L.jpg";
-  $phimg = "{$link_name}/images/placeholder/1575466871112.jfif";
+  $bgimg = "images/econa/fields/15/com_content_article/{$bgimg_id}/{$bgimg_name}_L.jpg";
+  $phimg = "images/placeholder/1575466871112.jfif";
   $personimg_id = json_decode($article->jcfields[17]->rawvalue)->itemId;
   $personimg_name = json_decode($article->jcfields[17]->rawvalue)->filename;
-  $personimg = "{$link_name}/images/econa/fields/17/com_content_article/{$personimg_id}/{$personimg_name}_L.jpg";
+  $personimg = "images/econa/fields/17/com_content_article/{$personimg_id}/{$personimg_name}_L.jpg";
   $url_id = $article -> id;
   $cht = "qr";
   $chs = "300x300";
